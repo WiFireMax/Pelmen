da58902283362e401a354120385eb67626732434
WiFireMax <maxbakharev.nsk@gmail.com>
2023-10-15 16:39:30 +0700

    Lab02Progress1

--- Lab02/mainwindow.cpp
+++ Lab02/mainwindow.cpp
@@ -6,6 +6,39 @@
 float PL(float f, float d){
     return 28 + 22 * log10(f) + 20*log10(d);
 }
+int N_of_obstructions(QPainter* Pix, QPixmap* Map, int x0, int x1, int y0, int y1) {
+    int Number=0;
+    int deltax = abs(x1 - x0);
+    int deltay = abs(y1 - y0);
+    float error = 0;
+    float deltaerr = (deltay + 1) / (deltax + 1);
+    int y = y0;
+    int diry = y1 - y0;
+    if (diry > 0) {
+        diry = 1;
+    }
+    if (diry < 0) {
+        diry = -1;
+    }
+    for (int x=x0; x<x1; x++) {
+        QImage image = Map->toImage();
+        QRgb color=image.pixel(x, y);
+        int red = qRed(color);
+        int green = qGreen(color);
+        int blue = qBlue(color);
+        if (red==0 && green==0 && blue==0) {
+            Number+=1;
+        }
+        Pix->setPen(QColor(0, 100, 0, 255));
+        Pix->drawPoint(x, y);
+        error = error + deltaerr;
+        if (error >= 1.0) {
+            y = y + diry;
+            error = error - 1.0;
+        }
+    }
+    return Number;
+}
 struct Obstruction { //препятствия
     struct Material {
         int irrgrass; //IRR стекло
@@ -35,6 +68,62 @@ struct Obstruction { //препятствия
                 error = error - 1.0;
             }
         }
+        return 0;
+    }
+    int circle(QPainter* Pix, int X1, int Y1, int R) {
+        int x = 0;
+        int y = R;
+        int delta = 1 - 2 * R;
+        int error = 0;
+        while (y >= x) {
+            Pix->setPen(QColor(0,0,0,255));
+            Pix->drawPoint(X1 + x, Y1 + y);
+            Pix->drawPoint(X1 + x, Y1 - y);
+            Pix->drawPoint(X1 - x, Y1 + y);
+            Pix->drawPoint(X1 - x, Y1 - y);
+            Pix->drawPoint(X1 + y, Y1 + x);
+            Pix->drawPoint(X1 + y, Y1 - x);
+            Pix->drawPoint(X1 - y, Y1 + x);
+            Pix->drawPoint(X1 - y, Y1 - x);
+            error = 2 * (delta + y) - 1;
+                if ((delta < 0) && (error <= 0)) {
+                    delta += 2 * ++x + 1;
+                    continue;
+                    }
+                if ((delta > 0) && (error > 0)) {
+                    delta -= 2 * --y + 1;
+                    continue;
+                }
+            delta += 2 * (++x - --y);
+        }
+        return 0;
+    }
+    int square(QPainter* Pix, int x0, int x1, int y0, int y1) {
+        Pix->setPen(QColor(0,0,0,255));
+        if (x0<x1) {
+            for (int x=x0; x<x1; x++) {
+                Pix->drawPoint(x, y0);
+                Pix->drawPoint(x, y1);
+                }
+            }
+        if (x1<x0) {
+            for (int x=x1; x<x0; x++) {
+                    Pix->drawPoint(x, y0);
+                    Pix->drawPoint(x, y1);
+            }
+        }
+        if (y0<y1) {
+            for (int y=y0; y<y1; y++) {
+                    Pix->drawPoint(x0, y);
+                    Pix->drawPoint(x1, y);
+                }
+            }
+        if (y1<y0) {
+            for (int y=y1; y<y0; y++) {
+                    Pix->drawPoint(x0, y);
+                    Pix->drawPoint(x1, y);
+                }
+            }
         return 0;
     }
 };
@@ -96,6 +185,13 @@ MainWindow::MainWindow(QWidget *parent)
     // /////////////////////////////////////////////////////////////////////////////////////
     struct Obstruction Stone;
     Stone.line(&p, 50, 400, 300, 800);
+    Stone.circle(&p, 300, 500, 200);
+    Stone.square(&p, 700, 900, 600, 800);
+    int Num=N_of_obstructions(&p, &map, cellPosX, 500, cellPosY, 500);
+    QString text=QString::number(Num);
+    QLabel *label = new QLabel();
+    label->setText(text);
+    label->show();
     // /////////////////////////////////////////////////////////////////////////////////////
     p.end();
     scene->addPixmap(map);
